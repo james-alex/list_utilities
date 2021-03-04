00b5103391e83fce579f4bb3175185e091ba76f9
James Alexander <j.alex@email.com>
2021-03-03 21:58:52 -0800

    Add the [getRandom] method.

--- lib/src/set_utilities.dart
+++ lib/src/set_utilities.dart
@@ -1,10 +1,26 @@
 import 'iterable_utilities.dart';
+import 'list_utilities.dart';
 
 /// Extends [Set] with additional basic methods.
 extension SetUtilities<T> on Set<T> {
-  /// Removes and returns a random value from the set.
+  /// Returns a random element from this set.
+  Set<T> getRandom(int count, [int seed]) {
+    assert(isNotEmpty, 'The set must not be empty.');
+    assert(count != null && count > 0 && count <= length);
+
+    final indexes = List<int>.generate(length, (index) => index);
+    final randomIndexes = <int>[];
+    for (var i = 0; i < length; i++) {
+      randomIndexes.add(indexes.removeRandom());
+    }
+
+    return List<T>.generate(count, (index) => elementAt(randomIndexes[index]))
+        .toSet();
+  }
+
+  /// Removes and returns a random element in this set.
   T removeRandom([int seed]) {
-    assert(isNotEmpty, 'The list must not be empty.');
+    assert(isNotEmpty, 'The set must not be empty.');
     final element = random(seed);
     remove(element);
     return element;
